9fbc5ee949c607b4003ecd5c41b1bf3bcc48a9f3
Aaron <aaronwenteler@gmail.com>
2023-11-04 13:37:29 +0000

    added method to query claude

--- models/claude.py
+++ models/claude.py
@@ -1,12 +1,42 @@
 from dotenv import load_dotenv
+from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
 
 import os
 import yaml
 
+
+class Claude(Anthropic):
+    def __init__(self):
+        super().__init__(api_key=self.get_api_key())
+
+    def get_api_key(self):
+        load_dotenv(dotenv_path=self.get_api_path())
+        api_key = os.getenv("CLAUDE_SECRET")
+        return api_key
+
+    def get_api_path(self):
+        # Load the path to your API configuration from config file
+        config = self.config()
+        api_path = config['paths']['secret']
+        return api_path
+
+    @staticmethod
+    def config():
+        with open('../configs/claude.yml', 'r') as file:
+            config = yaml.safe_load(file)
+        return config
+
+    def create_completion(self, prompt):
+        completion = self.completions.create(
+            model="claude-2",
+            max_tokens_to_sample=300,
+            prompt=prompt
+        )
+        return completion.completion
+
+
 if __name__ == "__main__":
-    load_dotenv()
-
-    with open('config.yml', 'r') as file:
-        data = yaml.safe_load(file)
-
-    api_key = os.getenv("CLAUDE_SECRET")
+    claude = Claude()
+    prompt = f"{HUMAN_PROMPT} How many toes do dogs have?{AI_PROMPT}"
+    response = claude.create_completion(prompt)
+    print(response)
